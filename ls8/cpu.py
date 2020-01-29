494a4802b060c1c21b8dfc08c0488a73a290540b
Kizmet <bryantpatton@gmail.com>
2020-01-28 17:45:39 -0800

    mult ops

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,6 +2,10 @@
 
 import sys
 
+LDI = 0b10000010  # load "immediate", store a value in a register, or "set this register to this value"
+PRN = 0b01000111  # print the numeric value stored in a register
+HLT = 0b00000001  # halt the CPU and exit the emulator
+
 
 class CPU:
     """Main CPU class."""
@@ -11,6 +15,14 @@ class CPU:
         self.ram = [0] * 256  # 256 bytes of memory
         self.reg = [0] * 8  # 8 general-purpose registers
         self.pc = 0
+        self.reg[7] = 255
+        self.hlt = False
+        self.ops = {
+            LDI: self.op_ldi,
+            PRN: self.op_prn,
+            HLT: self.op_hlt,
+            MUL: self.op_mul,
+        }
 
     def ram_read(self, address):
         return self.ram[address]
@@ -18,35 +30,37 @@ class CPU:
     def ram_write(self, value, address):
         self.ram[address] = value
 
-    def load(self):
+    def load(self, blocks):
         """Load a program into memory."""
 
         address = 0
-
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010,  # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111,  # PRN R0
-            0b00000000,
-            0b00000001,  # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        for block in blocks:
+            if (block[0] == "0") or (block[0] == "1"):
+                self.ram[address] = int(block[:8], 2)
+                # print(self.ram[address])
+                address += 1
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        # elif op == "SUB": etc
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
+
+    def op_ldi(self, address, value):
+        self.reg[address] = value
+
+    def op_prn(self, address, op_b):  # op a/b
+        print(self.reg[address])  # op_a acts as address
+
+    def op_hlt(self, op_a, op_b):
+        self.hlt = True
+
+    def op_mul(self, operand_a, operand_b):
+        self.alu("MUL", operand_a, operand_b)
 
     def trace(self):
         """
@@ -74,23 +88,34 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        LDI = 0b10000010  # load "immediate", store a value in a register, or "set this register to this value"
-        PRN = 0b01000111  # print the numeric value stored in a register
-        HLT = 0b00000001  # halt the CPU and exit the emulator
+        LDI = 0b10000010
+        PRN = 0b01000111
+        HLT = 0b00000001
 
-        running = True
-
-        while running:
-            IR = self.ram[self.pc]
+        while self.hlt == False:
+            ir = self.ram[self.pc]
 
             operand_a = self.ram_read(self.pc + 1)
             operand_b = self.ram_read(self.pc + 2)
+            op_size = ir >> 6
+            ins_set = ((ir >> 4) & 0b1) == 1
 
-            if IR == LDI:
-                self.reg[operand_a] = operand_b
-                self.pc += 3
-            elif IR == PRN:
-                print(self.reg[operand_a])
-                self.pc += 2
-            elif IR == HLT:
-                running = False
+            if ir in self.ops:
+                self.ops[ir](operand_a, operand_b)
+            if not ins_set:
+                self.pc += op_size + 1
+            self.trace()
+            # if not ins_set:
+            #     self.pc += op_size + 1
+
+            # if IR == LDI:
+            #     self.reg[operand_a] = operand_b
+            #     self.pc += 3
+            # elif IR == PRN:
+            #     print(self.reg[operand_a])
+            #     self.pc += 2
+            # elif IR == MUL:
+            #     self.reg[operand_a] *= operand_b
+            #     self.pc += 2
+            # elif IR == HLT:
+            #     running = False
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -1,11 +1,53 @@
 #!/usr/bin/env python3
+import argparse
 
 """Main."""
+
+# python3 ls8.py examples/mult.ls8
+
+"""python
+sys.argv[0] == "ls8.py"
+sys.argv[1] == "examples/mult.ls8"
+"""
 
 import sys
 from cpu import *
 
-cpu = CPU()
+parser = argparse.ArgumentParser()
+parser.add_argument("file", help="ops filename")
+args = parser.parse_args()
+filename = args.file
 
-cpu.load()
+
+# with open(filename, "rb") as f:
+#     read_data = f.read()
+# print(read_data)
+
+from functools import partial
+
+cpu = CPU()
+blocks = []
+with open(filename, "r") as f:
+    # for block in iter(partial(f.read, 8), b""):
+    # for row in f:
+    #     split = row.split("#")
+    #     rom = split[0]
+    #     if rom == "":
+    #         continue
+    #     else:
+    #         print(partial(rom, 8))
+    #         # if not row or row.startswith(b"#"):
+    #         #     blocks.append([])
+    #         # for block in iter(partial(rom, 8), b""):
+    #         blocks.append(rom)
+    for line in f:
+        comment_split = line.split("#")  # used to ignore comments
+        instruction = comment_split[0]
+        if instruction == "":
+            continue
+        else:
+            blocks.append(instruction[:8])
+
+# print(blocks)
+cpu.load(blocks)
 cpu.run()
